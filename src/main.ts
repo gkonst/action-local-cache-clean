4cdcc5ffeedf64f3c9476895c9f85c5f73500ecc
Konstantin Grigoriev <konstantin.v.grigoriev@gmail.com>
2024-06-20 19:40:43 +0300

    dist added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import * as core from '@actions/core'
-import {setFailed} from '@actions/core'
-import {isErrorLike} from './lib/isErrorLike'
-import {rmRF} from '@actions/io';
-import path from 'path';
+import { setFailed } from '@actions/core'
+import { isErrorLike } from './lib/isErrorLike'
+import { rmRF } from '@actions/io'
+import path from 'path'
 
-const {GITHUB_REPOSITORY, RUNNER_TOOL_CACHE} = process.env
+const { GITHUB_REPOSITORY, RUNNER_TOOL_CACHE } = process.env
 
 async function clean(dir: string) {
-  console.log("Cleaning dir: ", dir)
+  console.log('Cleaning dir: ', dir)
   await rmRF(dir)
 }
 
@@ -26,10 +26,10 @@ async function main(): Promise<void> {
       throw new TypeError('keys is required but was not provided.')
     }
 
-    console.info("Keys to be cleaned up: ", keys)
+    console.info('Keys to be cleaned up: ', keys)
 
     for (let key of keys) {
-      clean(path.join(RUNNER_TOOL_CACHE, GITHUB_REPOSITORY, key));
+      clean(path.join(RUNNER_TOOL_CACHE, GITHUB_REPOSITORY, key))
     }
   } catch (error: unknown) {
     console.trace(error)
