d7299c50d0b8aad3f0265351198abb4f1de4a9d0
Konstantin Grigoriev <konstantin.v.grigoriev@gmail.com>
2024-06-20 19:33:39 +0300

    Input parameters reworked

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
+import * as core from '@actions/core'
 import {setFailed} from '@actions/core'
-
-import {getVars, Vars} from './lib/getVars'
 import {isErrorLike} from './lib/isErrorLike'
 import {rmRF} from '@actions/io';
+import path from 'path';
 
-async function clean(vars: Vars) {
-  console.log("Cleaning dir: ", vars.cacheDir)
-  await rmRF(vars.cacheDir)
+const {GITHUB_REPOSITORY, RUNNER_TOOL_CACHE} = process.env
+
+async function clean(dir: string) {
+  console.log("Cleaning dir: ", dir)
+  await rmRF(dir)
 }
 
 async function main(): Promise<void> {
   try {
-    const vars = getVars();
-    clean(vars);
+    if (!RUNNER_TOOL_CACHE) {
+      throw new TypeError('Expected RUNNER_TOOL_CACHE environment variable to be defined.')
+    }
+
+    if (!GITHUB_REPOSITORY) {
+      throw new TypeError('Expected GITHUB_REPOSITORY environment variable to be defined.')
+    }
+    const keys = core.getMultilineInput('keys')
+
+    if (!keys) {
+      throw new TypeError('keys is required but was not provided.')
+    }
+
+    console.info("Keys to be cleaned up: ", keys)
+
+    for (let key of keys) {
+      clean(path.join(RUNNER_TOOL_CACHE, GITHUB_REPOSITORY, key));
+    }
   } catch (error: unknown) {
     console.trace(error)
     setFailed(isErrorLike(error) ? error.message : `unknown error: ${error}`)
